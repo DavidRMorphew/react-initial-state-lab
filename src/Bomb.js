035322a438c095bba69d450cd7e438f58fc0e754
DavidRMorphew <morphewd@umich.edu>
2021-05-20 20:45:15 -0400

    commit tree e4224e34f5204e148cca06adc2f72e65f9ebd4a0

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
 // your Bomb code here!
+import React, { Component } from 'react'
+class Bomb extends Component {
+    state = {
+        secondsLeft: this.props.initialCount
+    }
+
+    boomOrSecondsLeft = () => {
+        return (this.state.secondsLeft === 0) ? "Boom!" : this.state.secondsLeft + ' seconds left before I go boom!'
+    }
+    
+    render(){
+        return(
+            <div>{this.boomOrSecondsLeft()}</div>
+        )
+    }
+}
+
+export default Bomb
